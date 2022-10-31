56b2fba877b6b8c5290a00a010f09d7006865760
likun <496546144@qq.com>
2022-10-31 21:10:16 +0800

    优化

--- app/src/main/java/com/ashlikun/keeplive/simple/MainActivity.kt
+++ app/src/main/java/com/ashlikun/keeplive/simple/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.ashlikun.keeplive.simple
 
 import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.ashlikun.keeplive.KeepLive
@@ -18,6 +20,12 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     val binding by lazy {
         MainViewgroupActivityBinding.inflate(layoutInflater)
+    }
+    val pm by lazy {
+        getSystemService(Context.POWER_SERVICE) as PowerManager
+    }
+    val wakeLock by lazy {
+        pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "KeepLive:WakeLock")
     }
 
     fun getFormatTime(calendar: Calendar?, format: String = "yyyy-MM-dd HH:mm:ss") =
@@ -44,10 +52,7 @@ class MainActivity : AppCompatActivity() {
             if (Build.VERSION.SDK_INT >= 29) {
                 // Android10.0之后使用计步器需要健身运动权限
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED) {
-                    KeepLive.startWork(application, ForegroundNotification("测试Keep", "描述", R.mipmap.ic_launcher))
-                    Test.get().start {
-                        binding.textView.text = it
-                    }
+                    start()
                 } else {
                     requestPermissions(arrayOf(Manifest.permission.ACTIVITY_RECOGNITION), 111)
                 }
@@ -56,18 +61,29 @@ class MainActivity : AppCompatActivity() {
         }
         binding.textView2.setOnClickListener {
             KeepLive.stopWork(application)
+            wakeLock.release()
         }
+        KeepLive.foregroundNotification = ForegroundNotification("测试Keep", "描述", R.mipmap.ic_launcher)
 //        KeepLive.startWork(application, ForegroundNotification("测试Keep", "描述", R.mipmap.ic_launcher))
 //        Test.get().start()
+    }
+
+    private fun start() {
+        //                    KeepLive.startWork(application, ForegroundNotification("测试Keep", "描述", R.mipmap.ic_launcher))
+
+        if (!wakeLock.isHeld) {
+            //申请一个永久锁
+            wakeLock.acquire()
+        }
+        Test.get().start {
+            binding.textView.text = it
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == 111 && grantResults.find { it != PackageManager.PERMISSION_GRANTED } == null) {
-            KeepLive.startWork(application, ForegroundNotification("测试Keep", "描述", R.mipmap.ic_launcher))
-            Test.get().start {
-                binding.textView.text = it
-            }
+            start()
         }
     }
 
--- app/src/main/java/com/ashlikun/keeplive/simple/MyApp.kt
+++ app/src/main/java/com/ashlikun/keeplive/simple/MyApp.kt
@@ -31,7 +31,6 @@ class MyApp : Application() {
         appStart++
         registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
             override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-                if(activity.intent.getStr)
                 if (!activitys.contains(activity))
                     activitys.add(activity)
             }
